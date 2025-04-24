283422beb8c6757f5c706b8d26d5fcdcdee90d77
byMagg <byMag@hotmail.es>
2025-04-24 13:54:22 +0200

    cleanup

--- src/stores/session.ts
+++
@@ -1,4 +0,0 @@
-import Cookies from "js-cookie";
-import { atom } from "nanostores";
-
-export const sessionCookie = atom(Cookies.get("__session"));
--- src/stores/userLeagues.ts
+++
@@ -1,8 +0,0 @@
-import { type League } from "@/types";
-import { atom } from "nanostores";
-
-export const userLeagues = atom<League[]>([]);
-
-export const setUserLeagues = (leagues: League[]) => {
-  userLeagues.set(leagues);
-};
