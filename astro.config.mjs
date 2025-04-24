07cf1f64e87c9a7aef3906ce907b4d6a52817fd5
byMagg <byMag@hotmail.es>
2025-04-24 13:57:17 +0200

    eliminar componentes de formulario de inicio de sesión y registro, y agregar flujo de trabajo de despliegue a GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,14 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
+  site: "https://bymagg.github.io",
+  base: "/tfm-static",
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
   ],
-  output: "server",
-  adapter: vercel(),
-  session: {
-    driver: "memory",
-  },
 });
